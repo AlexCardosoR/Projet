cd92ca0196659d33e74d83b313a70c1f644eba6a
Cardoso Alex <33517600+AlexCardosoR@users.noreply.github.com>
2018-02-01 15:08:19 +0100

    v

--- JavaScript/TP1.js
+++ JavaScript/TP1.js
@@ -1,5 +1,25 @@
-var personne = {
-    getAdresse: function(){
-        return new adresse
-    }
-}
+// var metier = {
+//     adresse = {
+//     	numero : '1',
+//     	nom : '11 Novembre'
+//     }
+
+//     getAdresse : function(){
+//         return new adresse
+//     }
+// }
+
+// var vue = {
+// 	getHtml : function(){
+// 		return "<br/> adresse : " + metier.getAdresse.numero + "rue" + metier.getAdresse.nom + "<br/>" ;
+// 	}
+// }
+
+
+
+// var metier = new Object();
+// codeHtml = vue.getHtml();
+// document.getElementById("resultat").innerHtml = codeHtml;
+
+codeHtml="Hello"
+document.getElementById("resultat").innerHtml = codeHtml;
